bb44533c976557c10e027e536836fbcb253fde48
Michal Makuch <michalmakuch@hotmail.com>
2023-04-10 17:51:06 +0200

    feat: feeding implemented

--- js/game.js
+++ js/game.js
@@ -1,16 +1,22 @@
 import Tamagotchi from "./modules/tamagotchi.js";
+import Abilities from "./modules/abilities.js";
 
 export default class Game {
   constructor() {
-    this.tamagotchi = new Tamagotchi();
+    this.tamagotchi = new Tamagotchi(this.abilities);
+    this.abilities = new Abilities(this.tamagotchi);
   }
 
-  start = ({ healthElement, hungerElement, energyElement, funElement }) => {
+  start = ({ healthElement, hungerElement, energyElement, funElement, feedingButton }) => {
     this.tamagotchi.mount({
       healthElement: document.querySelector(`${healthElement}`),
       hungerElement: document.querySelector(`${hungerElement}`),
       energyElement: document.querySelector(`${energyElement}`),
       funElement: document.querySelector(`${funElement}`),
+    });
+
+    this.abilities.mount({
+      feedingButton: document.querySelector(`${feedingButton}`),
     });
     console.log("Game started");
   };
